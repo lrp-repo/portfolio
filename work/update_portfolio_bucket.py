1d8ffeaf9ddf9b60d105722c0cfd217d04febcb4
lrp-repo <larry@lpasf.org>
2018-12-05 12:52:53 -0500

    Working version 3.7 Lambda job

--- work/update_portfolio_bucket.py
+++ work/update_portfolio_bucket.py
@@ -1,22 +1,29 @@
+import json
 import boto3
 import io
 import zipfile
 import mimetypes
 
-# zip_file_name = 'portfoliobuild.zip'
+def lambda_handler(event, context):
+    
+    s3 = boto3.resource('s3')
+    
+    portfolio_bucket = s3.Bucket('portfolio.lpasf.org')
+    build_bucket = s3.Bucket('portfoliobuild.lpasf.org')
+    
+    portfolio_zip = io.BytesIO()
+    
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            content_type = mimetypes.guess_type(nm)[0]
+            print(content_type)
+            portfolio_bucket.upload_fileobj(obj, nm ,ExtraArgs={'ACL': 'public-read', 'ContentType': content_type })
+     
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Portfolio Lambda Job Completed Sucessfully')
+    }
 
-s3 = boto3.resource('s3')
-
-portfolio_bucket = s3.Bucket('portfolio.lpasf.org')
-build_bucket = s3.Bucket('portfoliobuild.lpasf.org')
-
-portfolio_zip = io.BytesIO()
-
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        content_type = mimetypes.guess_type(nm)[0]
-        # print(content_type)
-        portfolio_bucket.upload_fileobj(obj, nm ,ExtraArgs={'ACL': 'public-read', 'ContentType': content_type })
